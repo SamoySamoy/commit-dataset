6a211173140cb8b00a03bf266b466968a9b9c0f0
Dung Tran <antalois23@gmail.com>
2023-03-16 14:53:34 +0700

    modularize and fix altering dataset

---
+++ crawl.py
@@ -0,0 +1,79 @@
+import requests
+import csv
+import re
+from pattern import bot_message_patterns, bot_names_pattern, bot_emails_pattern, ex_pattern
+
+def crawl_commits(repo_owner, repo_name):
+    # Set up variables
+    repo_owner = repo_owner
+    repo_name = repo_name
+    api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
+    headers = {"Accept": "application/vnd.github.v3+json"}
+    params = {"per_page": 100}  # Number of commits to fetch per page
+    auth_token = "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"
+
+    # Make API request to get commits
+    response = requests.get(api_url, headers=headers, params=params, auth=(auth_token, ""))
+    commits = response.json()
+    all_commits = []
+
+    # # Fetch all pages of commits
+    # while response.status_code == 200:
+    #     all_commits.extend(commits)
+    #     link_header = response.headers.get("Link")
+    #     if link_header is None:
+    #         break
+    #     links = link_header.split(",")
+    #     for link in links:
+    #         if "rel=\"next\"" in link:
+    #             next_url = link.split(";")[0].strip("<>")
+    #             response = requests.get(next_url, headers=headers, params=params, auth=(auth_token, ""))
+    #             commits = response.json()
+    #             break
+    # Extract commit details and save them in CSV file
+
+    return commits
+
+def expand_dataset(commits):
+    # find the current id (number of rows)
+    with open("dataset.csv", mode="r", newline="") as csv_file:
+        reader = csv.reader(csv_file)
+        cur_id = sum(1 for row in reader) - 1
+
+
+    # add new data to dataset
+    with open("dataset.csv", mode="a", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        id = cur_id
+        for commit in commits:
+            id += 1
+            sha = commit["sha"]
+            author_name = commit["commit"]["author"]["name"]
+            author_email = commit["commit"]["author"]["email"]
+            message = commit["commit"]["message"]
+            summa = "None"
+
+            # Check if commit is from a bot
+            is_important = 1
+            for bot_name in bot_names_pattern:
+                if bot_name.lower() in author_name.lower() or ex_pattern.search(author_name):
+                    is_important = 0.5
+                    break
+            for bot_email in bot_emails_pattern:
+                if bot_email in author_email or ex_pattern.search(author_email):
+                    is_important = 0.5
+                    break
+            for pattern in bot_message_patterns:
+                if pattern.search(message):
+                    is_important = 0.5
+                    break
+
+            new_row = [id, sha, author_name, author_email, message, is_important, summa]
+            writer.writerow([])
+            writer.writerow(new_row)
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,80 +1,10 @@
 import requests
 import csv
 import re
-# Set up variables
-repo_owner = "sous-chefs"
-repo_name = "aws"
-api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
-headers = {"Accept": "application/vnd.github.v3+json"}
-params = {"per_page": 100}  # Number of commits to fetch per page
-auth_token = "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"
+from crawl import crawl_commits, expand_dataset
 
-# Define bot-related keywords
-bot_names = ["bot", "build", "deploy", "jenkins", "travis", "circleci", "github", "gitlab"]
-bot_emails = ["noreply@github.com", "noreply@github.com.invalid"]
-bot_patterns = [re.compile(r"\[skip ci\]", re.IGNORECASE),
-                re.compile(r"\[ci skip\]", re.IGNORECASE),
-                re.compile(r"no\s+tests?", re.IGNORECASE),
-                re.compile(r"auto-generated", re.IGNORECASE),
-                re.compile(r"automated\s+build", re.IGNORECASE),
-                re.compile(r"continuous\s+integration", re.IGNORECASE),
-                re.compile(r"test\s+automation", re.IGNORECASE),
-                re.compile(r"ci/cd", re.IGNORECASE),
-                re.compile(r"pipeline", re.IGNORECASE),
-                re.compile(r"dependenc", re.IGNORECASE),
-                re.compile(r"jenkinsfile", re.IGNORECASE),
-                re.compile(r"travis\.yml", re.IGNORECASE),
-                re.compile(r"circle\.yml", re.IGNORECASE),
-                re.compile(r"bump", re.IGNORECASE),
-                re.compile(r"gitlab-ci\.yml", re.IGNORECASE)]
-ex_pattern = re.compile(r"\b.*bot.*\b", re.IGNORECASE)
+if __name__ == "__main__":
+    commits = crawl_commits("theodorusclarence", "ts-nextjs-tailwind-starter")
+    expand_dataset(commits) # id,sha,author_name,author_email,message,is_important,summa
 
-# Make API request to get commits
-response = requests.get(api_url, headers=headers, params=params, auth=(auth_token, ""))
-commits = response.json()
-all_commits = []
-count = 0
-# # Fetch all pages of commits
-# while response.status_code == 200:
-#     all_commits.extend(commits)
-#     link_header = response.headers.get("Link")
-#     if link_header is None:
-#         break
-#     links = link_header.split(",")
-#     for link in links:
-#         if "rel=\"next\"" in link:
-#             next_url = link.split(";")[0].strip("<>")
-#             response = requests.get(next_url, headers=headers, params=params, auth=(auth_token, ""))
-#             commits = response.json()
-#             break
-
-# Extract commit details and save them in CSV file
-with open("dataset.csv", mode="w", newline="") as csv_file:
-    fieldnames = ["id", "sha", "author_name", "author_email", "message", "is_bot"]
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-    writer.writeheader()
-    id = 0
-    for commit in commits:
-        id += 1
-        sha = commit["sha"]
-        author_name = commit["commit"]["author"]["name"]
-        author_email = commit["commit"]["author"]["email"]
-        message = commit["commit"]["message"]
-
-        # Check if commit is from a bot
-        is_bot = False
-        for bot_name in bot_names:
-            if bot_name.lower() in author_name.lower() or ex_pattern.search(author_name):
-                is_bot = True
-                break
-        for bot_email in bot_emails:
-            if bot_email in author_email or ex_pattern.search(author_email):
-                is_bot = True
-                break
-        for pattern in bot_patterns:
-            if pattern.search(message):
-                is_bot = True
-                break
-        writer.writerow({"id": id, "sha": sha, "author_name": author_name, "author_email": author_email, "message": message, "is_bot": is_bot})
 
