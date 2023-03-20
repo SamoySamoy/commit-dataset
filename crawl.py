873fc84d9e8a91375c6ceb773ab806cd513c0f85
Dung Tran <antalois23@gmail.com>
2023-03-20 17:40:29 +0700

    calculate importance score using spacy and keywords, combine with bot detection

--- crawl.py
+++ crawl.py
@@ -1,36 +1,41 @@
 import requests
 import csv
 import re
+import spacy
 from pattern import bot_message_patterns, bot_names_pattern, bot_emails_pattern, ex_pattern
 
-def crawl_commits(repo_owner, repo_name):
+nlp = spacy.load('en_core_web_sm')
+
+def calculate_important_score(text):
+    # Parse the text with Spacy
+    doc = nlp(text)
+    # Define the important parts of speech
+    important_pos = ["NOUN", "VERB", "ADJ"]
+    keywords = ["fix", "add", "security", "fixed", "sync", "remove"]
+    # Calculate the importance score for the sentence
+    score = 0
+    length = len(text)
+    important_words = sum(1 for token in doc if token.pos_ in important_pos)
+    keywords = sum(1 for token in doc if token.text.lower() in keywords)
+    score = (important_words * 2) + keywords - length
+    if score >= 3:
+        return True
+    else:
+        return False
+
+
+def crawl_commits(repo_owner, repo_name): # need fixed to crawl all commits one time
     # Set up variables
     repo_owner = repo_owner
     repo_name = repo_name
     api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
     headers = {"Accept": "application/vnd.github.v3+json"}
-    params = {"per_page": 30}  # Number of commits to fetch per page
-    auth_token = "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"
+    params = {"per_page": 100}  # Number of commits to fetch per page
+    auth_token = "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"
 
     # Make API request to get commits
     response = requests.get(api_url, headers=headers, params=params, auth=(auth_token, ""))
     commits = response.json()
-    all_commits = []
-
-    # # Fetch all pages of commits
-    # while response.status_code == 200:
-    #     all_commits.extend(commits)
-    #     link_header = response.headers.get("Link")
-    #     if link_header is None:
-    #         break
-    #     links = link_header.split(",")
-    #     for link in links:
-    #         if "rel=\"next\"" in link:
-    #             next_url = link.split(";")[0].strip("<>")
-    #             response = requests.get(next_url, headers=headers, params=params, auth=(auth_token, ""))
-    #             commits = response.json()
-    #             break
-    # Extract commit details and save them in CSV file
 
     return commits
 
@@ -51,23 +56,37 @@ def expand_dataset(commits, file):
             author_email = commit["commit"]["author"]["email"]
             message = commit["commit"]["message"]
             summa = "None"
+            type = "None"
 
-            # Check if commit is from a bot
+            # Find important ratio, default by 1
+            # If we check that commit is from bot, decrease 0.3
+            # Using spacy to tokenize a commit message then check it's importance by
+            # calculate key words
             is_important = 1
+            is_bot = False
             for bot_name in bot_names_pattern:
                 if bot_name.lower() in author_name.lower() or ex_pattern.search(author_name):
-                    is_important = 0.7
+                    is_bot = True
                     break
             for bot_email in bot_emails_pattern:
                 if bot_email in author_email or ex_pattern.search(author_email):
-                    is_important = 0.7
+                    is_bot = True
                     break
             for pattern in bot_message_patterns:
                 if pattern.search(message):
-                    is_important = 0.7
+                    is_bot = True
                     break
 
-            new_row = [id, sha, author_name, author_email, message, is_important, summa]
+            if is_bot:
+                is_important -= 0.3
+
+            if calculate_important_score(message) == False:
+                if is_bot == False:
+                    is_important -= 0.3
+                else:
+                    is_important -= 0.5
+
+            new_row = [id, sha, author_name, author_email, message, round(is_important,1), summa, type]
             writer.writerow([])
             writer.writerow(new_row)
 
