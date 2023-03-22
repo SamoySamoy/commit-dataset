a1acc82b2dbea7a6005ba0e473420a356841ecc9
lvdthieu <luuvanducthieu291@gmail.com>
2023-03-22 08:12:57 +0700

    Change crawl function to crawl all commits

--- crawl.py
+++ crawl.py
@@ -1,17 +1,45 @@
 import requests
 import csv
-import re
 import spacy
 from pattern import bot_message_patterns, bot_names_pattern, bot_emails_pattern, ex_pattern
+from markdown import markdown
+from bs4 import BeautifulSoup
+import os.path
 
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_lg')
+github_token = "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"
+headers = {
+    'Authorization': f'token {github_token}',
+    'Accept': 'application/vnd.github.v3+json'
+}
+
+
+def crawl_commits(repo_owner, repo_name):
+    # Set up variables
+    global headers
+    page = 1
+    all_commits = []
+    # Make API request to get commits
+    while True:
+        url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits?per_page=100&page={page}"
+        response = requests.get(url, headers=headers)
+        commits = response.json()
+        commits_per_page = list(map(lambda x: [x["sha"], x["commit"]["author"]["name"], x["commit"]["author"]["email"],
+                                               x["commit"]["message"]], commits))
+        all_commits += commits_per_page
+        if len(commits_per_page) < 100:
+            break
+        page += 1
+    return all_commits
+
 
 def calculate_important_score(text):
     # Parse the text with Spacy
     doc = nlp(text)
     # Define the important parts of speech
     important_pos = ["NOUN", "VERB", "ADJ"]
-    keywords = ["fix", "add", "security", "fixed", "sync", "remove", "update", "migrate", "feature", "feat", "change", "changes", "refactor"]
+    keywords = ["fix", "add", "security", "fixed", "sync", "remove", "update", "migrate", "feature", "feat", "change",
+                "changes", "refactor"]
     # Calculate the importance score for the sentence
     score = 0
     length = len(doc)
@@ -22,44 +50,30 @@ def calculate_important_score(text):
         return False
     return score >= 0
 
-def crawl_commits(repo_owner, repo_name): # need fixed to crawl all commits one time
-    # Set up variables
-    repo_owner = repo_owner
-    repo_name = repo_name
-    api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
-    headers = {"Accept": "application/vnd.github.v3+json"}
-    params = {"per_page": 100}  # Number of commits to fetch per page
-    auth_token = "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"
-
-    # Make API request to get commits
-    response = requests.get(api_url, headers=headers, params=params, auth=(auth_token, ""))
-    commits = response.json()
-
-    return commits
 
 def expand_dataset(commits, file):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    count = 0
     # find the current id (number of rows)
     with open(file, mode="r", newline="") as csv_file:
         reader = csv.reader(csv_file)
-        cur_id = sum(1 for row in reader) - 1
-
+        for count, line in enumerate(reader):
+            pass
+    print(count)
     # add new data to dataset
-    with open(file, mode="a", newline="") as csv_file:
+    with open(file, mode="a", encoding="utf-8", newline="") as csv_file:
         writer = csv.writer(csv_file)
-        id = cur_id
+        index = count
         for commit in commits:
-            id += 1
-            sha = commit["sha"]
-            author_name = commit["commit"]["author"]["name"]
-            author_email = commit["commit"]["author"]["email"]
-            message = commit["commit"]["message"]
+            index += 1
+            sha = commit[0]
+            author_name = commit[1]
+            author_email = commit[2]
+            message = commit[3]
             summa = "None"
-            type = "None"
-
-            # Find important ratio, default by 1
-            # If we check that commit is from bot, decrease 0.3
-            # Using spacy to tokenize a commit message then check it's importance by
-            # calculate key words
+            _type = "None"
+            # Find important ratio, default by 1. If we check that commit is from bot, decrease important ratio 0.3
+            # Using spacy to tokenize a commit message then check if it is important by calculating keywords
             is_important = 1
             is_bot = False
             for bot_name in bot_names_pattern:
@@ -74,24 +88,13 @@ def expand_dataset(commits, file):
                 if pattern.search(message):
                     is_bot = True
                     break
-
             if is_bot:
                 is_important -= 0.3
-
-            if calculate_important_score(message) == False:
-                if is_bot == False:
-                    is_important -= 0.3
-                else:
-                    is_important -= 0.5
+            if calculate_important_score(message) is False:
+                is_important = is_important - 0.3 if not is_bot else is_important - 0.5
             if is_important == 1:
                 print(message)
-            new_row = [id, sha, author_name, author_email, message, round(is_important,1), summa, type]
+            new_row = [index, sha, author_name, author_email, message, round(is_important, 1), summa, _type]
             writer.writerow([])
             writer.writerow(new_row)
-
-
-
-
-
-
 
--- pattern.py
+++ pattern.py
@@ -1,24 +1,23 @@
 import re
 # Define bot-related keywords
-ex_pattern = re.compile(r"\b.*bot.*\b", re.IGNORECASE) # check if name or email contains "bot" not a single word
+# check if name or email contains "bot" not a single word
+ex_pattern = re.compile(r"\b.*bot.*\b", re.IGNORECASE)
 bot_names_pattern = ["bot", "build", "deploy", "jenkins", "travis", "circleci", "github", "gitlab"]
 bot_emails_pattern = ["noreply@github.com", "noreply@github.com.invalid"]
-bot_message_patterns = [re.compile(r"\[skip ci\]", re.IGNORECASE),
-                re.compile(r"\[ci skip\]", re.IGNORECASE),
-                re.compile(r"no\s+tests?", re.IGNORECASE),
-                re.compile(r"auto-generated", re.IGNORECASE),
-                re.compile(r"automated\s+build", re.IGNORECASE),
-                re.compile(r"continuous\s+integration", re.IGNORECASE),
-                re.compile(r"test\s+automation", re.IGNORECASE),
-                re.compile(r"ci/cd", re.IGNORECASE),
-                re.compile(r"pipeline", re.IGNORECASE),
-                re.compile(r"dependenc", re.IGNORECASE),
-                re.compile(r"jenkinsfile", re.IGNORECASE),
-                re.compile(r"travis\.yml", re.IGNORECASE),
-                re.compile(r"circle\.yml", re.IGNORECASE),
-                re.compile(r"bump", re.IGNORECASE),
-                re.compile(r"chore", re.IGNORECASE),
-                re.compile(r"merge", re.IGNORECASE),
-                re.compile(r"gitlab-ci\.yml", re.IGNORECASE)]
-
-
+bot_message_patterns = [re.compile(r"\[skip ci]", re.IGNORECASE),
+                        re.compile(r"\[ci skip]", re.IGNORECASE),
+                        re.compile(r"no\s+tests?", re.IGNORECASE),
+                        re.compile(r"auto-generated", re.IGNORECASE),
+                        re.compile(r"automated\s+build", re.IGNORECASE),
+                        re.compile(r"continuous\s+integration", re.IGNORECASE),
+                        re.compile(r"test\s+automation", re.IGNORECASE),
+                        re.compile(r"ci/cd", re.IGNORECASE),
+                        re.compile(r"pipeline", re.IGNORECASE),
+                        re.compile(r"dependenc", re.IGNORECASE),
+                        re.compile(r"jenkinsfile", re.IGNORECASE),
+                        re.compile(r"travis\.yml", re.IGNORECASE),
+                        re.compile(r"circle\.yml", re.IGNORECASE),
+                        re.compile(r"bump", re.IGNORECASE),
+                        re.compile(r"chore", re.IGNORECASE),
+                        re.compile(r"merge", re.IGNORECASE),
+                        re.compile(r"gitlab-ci\.yml", re.IGNORECASE)]
